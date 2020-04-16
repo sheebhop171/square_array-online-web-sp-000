a0ead502001ec70e6ac0ed4ad3a2c483292ba99d
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-16 20:43:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,8 @@
+#def square_array(array)
+#  array.each do |i|
+#    i ** 2
+#  end
+#end
 def square_array(array)
-  array.each do |i|
-    i ** 2
-  end
+  j.each_with_index { |e, i| array[i] = e**2 }
 end
