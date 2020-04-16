f4ef2ee470312bda0825ff8c976d0030cf49a366
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-16 21:03:49 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+#def square_array(array)
+#  array.each do |i|
+#  new_array = i ** 2
+#  end
+#end
+
 def square_array(array)
-  # your code here
+    new_array = []
+    array.each { |e| new_array << e ** 2 }
+    new_array
 end
